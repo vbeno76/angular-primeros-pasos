090283f0cd21d24002b59b3f1555b7c84e9aebf2
Luis Escobedo <lescobedo@asecam.com.mx>
2024-02-22 12:19:28 -0600

    4 Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/components/heroes.module';
+import { CounterModule } from './counter/counter.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule,
   ],
   providers: [
     provideClientHydration()
